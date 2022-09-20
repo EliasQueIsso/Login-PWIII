c56bca4ed3b1e9e3ff3ec2f72a1243c40b867a5e
Elias apenas <elias2004souzam@gmail.com>
2022-09-20 14:35:04 -0300

    Adição de um bootstrap

---
+++ Login/about.php
@@ -0,0 +1,173 @@
+<?php include("cab.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Sobre Nós
+                    <small>Etec Albert Einstein</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a>
+                    </li>
+                    <li class="active">Sobre</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="toqueNaRede.png" alt="">
+            </div>
+            <div class="col-md-6">
+                <h2>TOQUE NA REDE</h2>
+                <p>O objetivo do nosso projeto é trazer o esporte da praia para a cidade, no sentido de facilitar o consumo de arenas de praia na cidade grande.</p>
+                <p>A diversão da praia no meio da cidade.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Nosso grupo de TCC</h2>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="Cleo.jpeg" alt="">
+                    <div class="caption">
+                        <h3>Cléo Leal<br>
+                            <small>PO & DEV</small>
+                        </h3>
+                        <ul class="list-inline">
+                            <li><a href="https://www.instagram.com/cleo.leal/"><i class="fa fa-2x fa-instagram"></i></class=></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/cleo-l-67b124243/"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            <li><a href="https://www.github.com/CleoLeal"><i class="fa fa-2x fa-github"></i></class=></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="Elias.jpeg" alt="">
+                    <div class="caption">
+                        <h3>Elias Mendonça<br>
+                            <small>Scrum Master & DEV</small>
+                        </h3>
+                        <ul class="list-inline">
+                            <li><a href="https://www.instagram.com/elias__apenas/"><i class="fa fa-2x fa-instagram"></i></class=></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/elias-de-souza-mendon%C3%A7a-7b39a5229/"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            <li><a href="https://github.com/EliasQueIsso"><i class="fa fa-2x fa-github"></i></class=></a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="Cristian.jpeg" alt="">
+                    <div class="caption">
+                        <h3>Cristian Gonçalves<br>
+                            <small>DEV</small>
+                        </h3>
+                        <ul class="list-inline">
+                            <li><a href="https://www.instagram.com/cristian_silva_tamaxi/"><i class="fa fa-2x fa-instagram"></i></class=></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/cristiangoncalves"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            <li><a href="https://github.com/cris-star-oss"><i class="fa fa-2x fa-github"></i></class=></a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="Rayssa.jpeg" alt="">
+                    <div class="caption">
+                        <h3>Rayssa Portugal<br>
+                            <small>DEV</small>
+                        </h3> 
+                        <ul class="list-inline">
+                            <li><a href="https://www.instagram.com/rayssa_anjos_portugal/"><i class="fa fa-2x fa-instagram"></i></class=></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/mwlite/in/rayssa-portugal-506a56250"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            <li><a href="https://github.com/RayssaPortugal"><i class="fa fa-2x fa-github"></i></class=></a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="Jullia.jpeg" alt="">
+                    <div class="caption">
+                        <h3>Jullia Barros<br>
+                            <small>DEV</small>
+                        </h3>
+ 
+                        <ul class="list-inline">
+                            <li><a href="https://www.instagram.com/jullia_barross_/"><i class="fa fa-2x fa-instagram"></i></class=></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/jullia-barros-a09051200"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            <li><a href="https://github.com/Jullia-Barros"><i class="fa fa-2x fa-github"></i></class=></a>
+                        </ul>
+                    </div>
+                </div>
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+
+        <!--
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Nossos Site</h2>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://placebacon.net/500/300" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://placebacon.net/500/300" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://placebacon.net/500/300" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://placebacon.net/500/300" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://placebacon.net/500/300" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="https://baconmockup.com/1200/300" alt="">
+            </div>
+        </div>
+        -->
+        <!-- /.row--> 
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Etec Albert Einstein website, 2022</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
